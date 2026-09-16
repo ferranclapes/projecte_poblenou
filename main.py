f949122012bd14fe9d884bfba5e0743ab672f93c
ferranclapes <114926403+ferranclapes@users.noreply.github.com>
2026-09-16 23:43:49 +0200

    CORS again

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origins_regex=r"https://([a-z0-9-]+)\.projecte-poblenou\.pages\.dev",
+    allow_origins_regex=r"https://.*\.projecte-poblenou\.pages\.dev",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
