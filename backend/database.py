a1cfaa64195c2cba3460e22687c161cdecad7425
ferranclapes <114926403+ferranclapes@users.noreply.github.com>
2026-09-16 22:23:55 +0200

    Connecció DB Aiven
    S'intenta accedir a la base de dades de Aiven. Si no es pot, accedeix a la local.

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/r1_database"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "mysql+pymysql://root:root@localhost:3306/r1_database"
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
